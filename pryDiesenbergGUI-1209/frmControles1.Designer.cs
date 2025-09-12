e506672f308cae083d5f30f7cb6d888ce8e7c46d
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-09-12 12:34:10 -0300

    cambie letrasa

--- pryDiesenbergGUI-1209/frmControles1.Designer.cs
+++ pryDiesenbergGUI-1209/frmControles1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblNombre1 = new System.Windows.Forms.Label();
             this.btn2 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
             this.lblNombre2 = new System.Windows.Forms.Label();
@@ -39,17 +39,19 @@
             this.txtAgregar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblNombre
+            // lblNombre1
             // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(117, 94);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(53, 13);
-            this.lblNombre.TabIndex = 1;
-            this.lblNombre.Text = "Nombre 1";
+            this.lblNombre1.AutoSize = true;
+            this.lblNombre1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre1.Location = new System.Drawing.Point(92, 93);
+            this.lblNombre1.Name = "lblNombre1";
+            this.lblNombre1.Size = new System.Drawing.Size(82, 18);
+            this.lblNombre1.TabIndex = 1;
+            this.lblNombre1.Text = "Nombre 1";
             // 
             // btn2
             // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2.Location = new System.Drawing.Point(95, 333);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(75, 23);
@@ -59,7 +61,8 @@
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(282, 333);
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(307, 333);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(75, 23);
             this.btn1.TabIndex = 3;
@@ -69,47 +72,53 @@
             // lblNombre2
             // 
             this.lblNombre2.AutoSize = true;
-            this.lblNombre2.Location = new System.Drawing.Point(117, 153);
+            this.lblNombre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre2.Location = new System.Drawing.Point(92, 156);
             this.lblNombre2.Name = "lblNombre2";
-            this.lblNombre2.Size = new System.Drawing.Size(53, 13);
+            this.lblNombre2.Size = new System.Drawing.Size(82, 18);
             this.lblNombre2.TabIndex = 4;
             this.lblNombre2.Text = "Nombre 2";
             // 
             // lblNombre3
             // 
             this.lblNombre3.AutoSize = true;
-            this.lblNombre3.Location = new System.Drawing.Point(117, 215);
+            this.lblNombre3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre3.Location = new System.Drawing.Point(92, 221);
             this.lblNombre3.Name = "lblNombre3";
-            this.lblNombre3.Size = new System.Drawing.Size(53, 13);
+            this.lblNombre3.Size = new System.Drawing.Size(82, 18);
             this.lblNombre3.TabIndex = 5;
             this.lblNombre3.Text = "Nombre 3";
             // 
             // txtNombre1
             // 
+            this.txtNombre1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre1.Location = new System.Drawing.Point(282, 87);
             this.txtNombre1.Name = "txtNombre1";
-            this.txtNombre1.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre1.Size = new System.Drawing.Size(100, 24);
             this.txtNombre1.TabIndex = 6;
             // 
             // txtNombre2
             // 
+            this.txtNombre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre2.Location = new System.Drawing.Point(282, 150);
             this.txtNombre2.Name = "txtNombre2";
-            this.txtNombre2.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre2.Size = new System.Drawing.Size(100, 24);
             this.txtNombre2.TabIndex = 7;
             // 
             // txtNombre3
             // 
+            this.txtNombre3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre3.Location = new System.Drawing.Point(282, 215);
             this.txtNombre3.Name = "txtNombre3";
-            this.txtNombre3.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre3.Size = new System.Drawing.Size(100, 24);
             this.txtNombre3.TabIndex = 8;
             // 
             // txtAgregar
             // 
-            this.txtAgregar.Location = new System.Drawing.Point(184, 269);
+            this.txtAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAgregar.Location = new System.Drawing.Point(196, 269);
             this.txtAgregar.Name = "txtAgregar";
-            this.txtAgregar.Size = new System.Drawing.Size(75, 23);
+            this.txtAgregar.Size = new System.Drawing.Size(75, 30);
             this.txtAgregar.TabIndex = 9;
             this.txtAgregar.Text = "Agregar";
             this.txtAgregar.UseVisualStyleBackColor = true;
@@ -127,7 +136,7 @@
             this.Controls.Add(this.lblNombre2);
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.btn2);
-            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblNombre1);
             this.Name = "frmControles1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Controles de Interfaz Gráfica";
@@ -137,7 +146,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblNombre1;
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Label lblNombre2;
