2d0ac6cf40d726b481e6dcfa7c28bf60acf6514a
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-09-17 12:41:56 -0300

    -

--- pryDiesenbergGUI-1209/frmControles1.Designer.cs
+++ pryDiesenbergGUI-1209/frmControles1.Designer.cs
@@ -71,11 +71,11 @@
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResultado.ForeColor = System.Drawing.Color.Indigo;
             this.lblResultado.Location = new System.Drawing.Point(214, 166);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 18);
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
             this.lblResultado.TabIndex = 11;
             // 
             // btnPrimero
